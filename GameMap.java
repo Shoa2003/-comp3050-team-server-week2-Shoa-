3c5512ea4c6af8df981e73484032098eeea9e656
JaeHyeok <jaehyeok.park@students.mq.edu.au>
2026-03-29 22:59:44 +1100

    feature: implement /move endpoint and query parsing

---
+++ GameMap.java
@@ -0,0 +1,14 @@
+// Temporary stub for MoveHandler testing
+// Arindam will overwrite this with the actual implementation later.
+public class GameMap {
+    
+    public static boolean isInBounds(int y, int x) {
+        // Assume true until map logic is implemented
+        return true; 
+    }
+    
+    public static boolean isBlocking(int y, int x) {
+        // Assume false (not blocking) until map logic is implemented
+        return false;
+    }
+}
--- Test.java
+++ Test.java
@@ -3,12 +3,21 @@ import com.sun.net.httpserver.HttpServer;
 
 public class Test {
 
+    // Initial player position
+    public static int playerY = 5;
+    public static int playerX = 5;
+
     public static void main(String[] args) throws Exception {
         HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+        
         server.createContext("/test", new MyHandler());
-        server.setExecutor(null); // creates a default executor
         server.createContext("/hello", new HelloHandler());
-        server.start();
-    }
+        
+        // Added MoveHandler for player movement
+        server.createContext("/move", new MoveHandler());
 
+        server.setExecutor(null); 
+        server.start();
+        System.out.println("Server started on port 8000...");
+    }
 }
